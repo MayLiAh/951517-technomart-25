c5b11cce64021e73f0f68e176ce598d51ad053a5
MayLiAh <may-goldenvol@yandex.ru>
2019-06-14 11:40:03 +0300

    поправлен поиск в шапке сайта

--- js/script.js
+++ js/script.js
@@ -1,27 +1,4 @@
-function getInternetExplorerVersion()
-{
-  var rv = -1;
-  if (navigator.appName == 'Microsoft Internet Explorer')
-  {
-    var ua = navigator.userAgent;
-    var re = new RegExp("MSIE ([0-9]{1,}[\\.0-9]{0,})");
-    if (re.exec(ua) != null)
-      rv = parseFloat( RegExp.$1 );
-  }
-  else if (navigator.appName == 'Netscape')
-  {
-    var ua = navigator.userAgent;
-    var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\\.0-9]{0,})");
-    if (re.exec(ua) != null)
-      rv = parseFloat( RegExp.$1 );
-  }
-  return rv;
-}
-
-alert(getInternetExplorerVersion());
-
-if (getInternetExplorerVersion() === -1) {
-  const mapButton = document.querySelector('.contacts-map');
+const mapButton = document.querySelector('.contacts-map');
 const mapModal = document.querySelector('.modal-map-full');
 const closeMapModalButton = document.querySelector('.modal-close-map');
 
@@ -95,4 +72,3 @@ document.addEventListener("keydown", function (event) {
     });
   }
 });  
-}
