68b91bd3e535ff2c10b212321acacfe470e3438b
MayLiAh <may-goldenvol@yandex.ru>
2019-06-14 11:22:14 +0300

    добавлена другая проверка на браузер

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-alert(navigator.appName);
+function getInternetExplorerVersion()
+{
+  var rv = -1;
+  if (navigator.appName == 'Microsoft Internet Explorer')
+  {
+    var ua = navigator.userAgent;
+    var re = new RegExp("MSIE ([0-9]{1,}[\\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  else if (navigator.appName == 'Netscape')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  return rv;
+}
+
+alert(getInternetExplorerVersion());
 
 const mapButton = document.querySelector('.contacts-map');
 const mapModal = document.querySelector('.modal-map-full');
